e2474a1e5ccfebeac3dc44068aa7d453c7b89b91
stellawoo <haidaiji@Haidais-MacBook-Pro.local>
2016-07-28 22:36:49 -0700

    modified2

--- api-ai-facebook/src/app.js
+++ api-ai-facebook/src/app.js
@@ -25,32 +25,10 @@ function sendFBMessage(sender, messageData, callback) {
         json: {
             recipient: {id: sender},
             message: {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-          {
-            "title":"Welcome to Peter\'s Hats",
-            "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-            "subtitle":"We\'ve got the right hat for everyone.",
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-                "title":"View Website"
-              },
-              {
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"USER_DEFINED_PAYLOAD"
-              }              
-            ]
-          }
-        ]
-      }
-    }
-  }
+
+                'text' : 'test'
+
+            }
         }
     }, (error, response, body) => {
         if (error) {
